6b10643c8bd58361f553fb125d91d508cdd0e56c
ote <ooottee@gmail.com>
2020-09-09 16:50:30 +1000

    Fixed bug when using different language version of Google map

--- routes/map.js
+++ routes/map.js
@@ -1,7 +1,10 @@
+// import Deshortifier from 'deshortify'
 var express = require('express')
 var router = express.Router()
 var Meta = require('html-metadata-parser')
+var request = require('request')
 
+// Take all the params from Url
 const parseQueryString = url => {
   var json = {}
   var arr = url.substr(url.indexOf('?') + 1).split('&')
@@ -12,19 +15,46 @@ const parseQueryString = url => {
   return json
 }
 
-// Short Url from Google
-router.get('/:shortId', function (req, res, next) {
-  const url = 'https://goo.gl/maps/' + req.params.shortId
-  // console.log('<------ url ------>\n', url)
-  Meta.parser(url, function (err, result) {
-    if (err) {
-      console.log('<------ ------>\n', err)
+// Change the url to China version
+const modifyUrl = url => {
+  var modifiedUrl = 'https://www.google.com/maps?'
+  var arr = url.substr(url.indexOf('?') + 1).split('&')
+  arr.forEach(item => {
+    var tmp = item.split('=')
+    if (tmp[0] === 'hl') {
+      tmp[1] = 'zh-Hans-CN'
+    } else if (tmp[0] === 'gl') {
+      tmp[1] = 'cn'
     }
-    const params = parseQueryString(result.og.image)
-    const cor = params.ll ? params.ll : params.center.replace('%2C', ',')
-    console.log('<------ paramas ------>\n', cor)
-    res.json(cor)
+    modifiedUrl = modifiedUrl + tmp[0] + '=' + tmp[1] + '&'
   })
+  return modifiedUrl
+}
+
+router.get('/:shortId', function (req, res) {
+  const uri = 'https://goo.gl/maps/' + req.params.shortId
+  request(
+    {
+      uri: uri,
+      followRedirect: false
+    },
+    function (err, httpResponse) {
+      if (err) {
+        return console.error(err)
+      }
+      var url = httpResponse.headers.location || uri
+      url = modifyUrl(url)
+      Meta.parser(url, function (err, result) {
+        if (err) {
+          console.log('<------ err ------>\n', err)
+        }
+        const params = parseQueryString(result.og.image)
+        const cor = params.ll ? params.ll : params.center.replace('%2C', ',')
+        console.log('<------ cor ------>\n', cor)
+        res.json(cor)
+      })
+    }
+  )
 })
 
 module.exports = router
